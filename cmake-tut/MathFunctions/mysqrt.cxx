876b4362f351c6d7ebe362c0df7801cbffab287b
zhangfeng775 <zhangfeng775@jk.cn>
2019-10-22 19:26:40 +0800

    Using system exp and log

--- cmake-tut/MathFunctions/mysqrt.cxx
+++ cmake-tut/MathFunctions/mysqrt.cxx
@@ -1,11 +1,18 @@
 #include <iostream>
+#include "TutorialConfig.h"
+#if defined(USE_EXP_LOG) && defined(HAVE_LOG) && defined(HAVE_EXP)
+#include <math.h>
+#endif
 
 double mysqrt(double x)
 {
     if (x <= 0) {
         return 0;
     }
-
+#if defined(USE_EXP_LOG) && defined(HAVE_LOG) && defined(HAVE_EXP)
+    double result = exp(log(x)*0.5);
+    std::cout << "Computing sqrt using exp() and log()" << std::endl;
+#else
     double result = x;
 
     for (int i = 0; i < 10; ++i) {
@@ -16,5 +23,6 @@ double mysqrt(double x)
         result = result + 0.5 * delta / result;
         std::cout << "Computing sqrt of " << x << " to be " << result << std::endl;
     }
+#endif
     return result;
 }
